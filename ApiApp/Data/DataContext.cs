354ed508cb40b746285c011820a70a0e066925f6
reaul1987 <reaul1987@gmail.com>
2022-09-26 17:13:56 -0500

    End of section two

---
+++ ApiApp/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<AppUser> Users { get; set; }
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ ApiApp/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiApp.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
