95ae6d47479ad2b88cf21eb1e208df7a0167eb90
reaul1987 <reaul1987@gmail.com>
2022-09-30 16:50:55 -0500

    End of section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './models/user';
+import { AccountService } from './services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -10,16 +12,14 @@ export class AppComponent implements OnInit {
   title = 'Hi world Datting App';
   users: any;
 
-  constructor(private http: HttpClient){}
+  constructor(private accountService: AccountService){}
 
   ngOnInit() {
-    this.getUsers();
+    this.setCurrentUser();
   }
 
-  getUsers() {
-    this.http.get('https://localhost:7210/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error)
-    })
+  setCurrentUser(){
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
 }
